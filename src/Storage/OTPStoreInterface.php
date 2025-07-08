631681d6c41d23a7233e3b5852fe5a2ced3c27b4
Shrikant Dharam <59720723+Shrikant-28@users.noreply.github.com>
2025-07-08 12:00:01 +0530

    Add phpdoc to OTPStoreInterface

--- src/Storage/OTPStoreInterface.php
+++ src/Storage/OTPStoreInterface.php
@@ -1,9 +1,39 @@
 <?php 
 namespace Shrikant\OTP\Storage;
 
+/**
+ * Interface OTPStoreInterface
+ *
+ * Defines the contract for storing and retrieving OTPs.
+ */
 interface OTPStoreInterface
 {
+    /**
+     * Stores an OTP with a specific key and time-to-live.
+     *
+     * @param string $key Unique identifier for the OTP.
+     * @param string $otp The OTP value to store.
+     * @param int $ttl Time-to-live in seconds.
+     *
+     * @return void
+     */
     public function set(string $key, string $otp, int $ttl): void;
+
+    /**
+     * Retrieves the OTP associated with a given key.
+     *
+     * @param string $key Unique identifier for the OTP.
+     *
+     * @return string|null The OTP if found, or null if not found or expired.
+     */
     public function get(string $key): ?string;
+
+    /**
+     * Deletes the OTP associated with a given key.
+     *
+     * @param string $key Unique identifier for the OTP.
+     *
+     * @return void
+     */
     public function delete(string $key): void;
 }
